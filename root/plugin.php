701e8b12f2846a9ba06fcbaa6344ade6cbe8a72d
Thorsten Ott <thorsten@thorsten-ott.de>
2013-11-10 15:08:06 +0100

    Adding a class based plugin skeleton with settings page

--- root/plugin.php
+++ root/plugin.php
@@ -29,50 +29,227 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-/**
- * Built using grunt-wp-plugin
- * Copyright (c) 2013 10up, LLC
- * https://github.com/10up/grunt-wp-plugin
- */
+class {%= class_name %} {
 
-// Useful global constants
-define( '{%= prefix_caps %}_VERSION', '0.1.0' );
-define( '{%= prefix_caps %}_URL',     plugin_dir_url( __FILE__ ) );
-define( '{%= prefix_caps %}_PATH',    dirname( __FILE__ ) . '/' );
+	private static $__instance = NULL;
 
-/**
- * Default initialization for the plugin:
- * - Registers the default textdomain.
- */
-function {%= prefix %}_init() {
-	$locale = apply_filters( 'plugin_locale', get_locale(), '{%= prefix %}' );
-	load_textdomain( '{%= prefix %}', WP_LANG_DIR . '/{%= prefix %}/{%= prefix %}-' . $locale . '.mo' );
-	load_plugin_textdomain( '{%= prefix %}', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	private $settings = array();
+	private $default_settings = array();
+	private $settings_texts = array();
+
+	private $plugin_prefix = '{%= prefix %}';
+	private $plugin_name = '{%= title %}';
+	private $settings_page_name = null;
+	private $dashed_name = '{%= name %}';
+	private $underscored_name = '{%= js_safe_name %}';
+	private $js_version = '{%= grunt.template.today('yymmddhhiiss') %}';
+	private $css_version = '{%= grunt.template.today('yymmddhhiiss') %}';
+
+	public function __construct() {
+		add_action( 'admin_init', array( &$this, 'register_setting' ) );
+		add_action( 'admin_menu', array( &$this, 'register_settings_page' ) );
+
+		/**
+		 * Default settings that will be used for the setup. You can alter these value with a simple filter such as this
+		 * add_filter( 'pluginprefix_default_settings', 'mypluginprefix_settings' );
+		 * function mypluginprefix_settings( $settings ) {
+		 * 		$settings['enable'] = false;
+		 * 		return $settings;
+		 * }
+		 */
+		$this->default_settings = (array) apply_filters( $this->plugin_prefix . '_default_settings', array(
+			'enable'				=> 1,
+		) );
+
+		/**
+		 * Define fields that will be used on the options page
+		 * the array key is the field_name the array then describes the label, description and type of the field. possible values for field types are 'text' and 'yesno' for a text field or input fields or 'echo' for a simple output
+		 * a filter similar to the default settings (ie pluginprefix_settings_texts) can be used to alter this values
+		 */
+		$this->settings_texts = (array) apply_filters( $this->plugin_prefix . '_settings_texts', array(
+			'enable' => array(
+				'label' => sprintf( __( 'Enable %s', $this->plugin_prefix ), $this->plugin_name ),
+				'desc' => sprintf( __( 'Enable %s', $this->plugin_prefix ), $this->plugin_name ),
+				'type' => 'yesno'
+			),
+		) );
+
+		$user_settings = get_option( $this->plugin_prefix . '_settings' );
+		if ( false === $user_settings )
+			$user_settings = array();
+
+		// after getting default settings make sure to parse the arguments together with the user settings
+		$this->settings = wp_parse_args( $user_settings, $this->default_settings );
+	}
+
+	public static function init() {
+		self::instance()->settings_page_name = sprintf( __( '%s Settings', self::instance()->plugin_prefix ), self::instance()->plugin_name );
+
+		if ( 1 == self::instance()->settings['enable'] ) {
+			add_action( 'init', self::instance()->init_hook_enabled() );
+		}
+		self::instance()->init_hook_always();
+	}
+
+	/*
+	 * Use this singleton to address methods
+	 */
+	public static function instance() {
+		if ( self::$__instance == NULL )
+			self::$__instance = new {%= class_name %};
+		return self::$__instance;
+	}
+
+	/**
+	 * Run these functions when the plugin is enabled
+	 */
+	public function init_hook_enabled() {
+
+	}
+
+	/**
+	 * Run these functions all the time
+	 */
+	public function init_hook_always() {
+		/**
+		 * If a css file for this plugin exists in ./css/wp-cron-control.css make sure it's included
+		 */
+		if ( file_exists( dirname( __FILE__ ) . "/css/" . $this->underscored_name . ".css" ) )
+			wp_enqueue_style( $this->dashed_name, plugins_url( "css/" . $this->underscored_name . ".css", __FILE__ ), $deps = array(), $this->css_version );
+		/**
+		 * If a js file for this plugin exists in ./js/wp-cron-control.css make sure it's included
+		 */
+		if ( file_exists( dirname( __FILE__ ) . "/js/" . $this->underscored_name . ".js" ) )
+			wp_enqueue_script( $this->dashed_name, plugins_url( "js/" . $this->underscored_name . ".js", __FILE__ ), array(), $this->js_version, true );
+
+		/**
+		 * Locale setup
+		 */
+		$locale = apply_filters( 'plugin_locale', get_locale(), $this->plugin_prefix );
+		load_textdomain( $this->plugin_prefix, WP_LANG_DIR . '/' . $this->plugin_prefix . '/' . $this->plugin_prefix . '-' . $locale . '.mo' );
+		load_plugin_textdomain( $this->plugin_prefix, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+	}
+
+	public function register_settings_page() {
+		add_options_page( $this->settings_page_name, $this->plugin_name, 'manage_options', $this->dashed_name, array( &$this, 'settings_page' ) );
+	}
+
+	public function register_setting() {
+		register_setting( $this->plugin_prefix . '_settings', $this->plugin_prefix . '_settings', array( &$this, 'validate_settings') );
+	}
+
+	public function validate_settings( $settings ) {
+		// reset to defaults
+		if ( !empty( $_POST[ $this->dashed_name . '-defaults'] ) ) {
+			$settings = $this->default_settings;
+			$_REQUEST['_wp_http_referer'] = add_query_arg( 'defaults', 'true', $_REQUEST['_wp_http_referer'] );
+
+		// or do some custom validations
+		} else {
+
+		}
+		return $settings;
+	}
+
+	public function settings_page() {
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not permission to access this page', $this->plugin_prefix ) );
+		}
+		?>
+		<div class="wrap">
+		<?php if ( function_exists('screen_icon') ) screen_icon(); ?>
+			<h2><?php echo $this->settings_page_name; ?></h2>
+
+			<form method="post" action="options.php">
+
+			<?php settings_fields( $this->plugin_prefix . '_settings' ); ?>
+
+			<table class="form-table">
+				<?php foreach( $this->settings as $setting => $value): ?>
+				<tr valign="top">
+					<th scope="row">
+						<label for="<?php echo $this->dashed_name . '-' . $setting; ?>">
+						<?php if ( isset( $this->settings_texts[$setting]['label'] ) ) {
+							echo $this->settings_texts[$setting]['label'];
+						} else {
+							echo $setting;
+						} ?>
+						</label>
+					</th>
+					<td>
+						<?php
+						/**
+						 * Implement various handlers for the different types of fields. This could be easily extended to allow for drop-down boxes, textareas and more
+						 */
+						?>
+						<?php switch( $this->settings_texts[$setting]['type'] ):
+							case 'yesno': ?>
+								<select name="<?php echo $this->plugin_prefix; ?>_settings[<?php echo $setting; ?>]" id="<?php echo $this->dashed_name . '-' . $setting; ?>" class="postform">
+									<?php
+										$yesno = array( 0 => __( 'No', $this->plugin_prefix ), 1 => __( 'Yes', $this->plugin_prefix ) );
+										foreach ( $yesno as $val => $txt ) {
+											echo '<option value="' . esc_attr( $val ) . '"' . selected( $value, $val, false ) . '>' . esc_html( $txt ) . "&nbsp;</option>\n";
+										}
+									?>
+								</select><br />
+							<?php break;
+							case 'text': ?>
+								<div><input type="text" name="<?php echo $this->plugin_prefix; ?>_settings[<?php echo $setting; ?>]" id="<?php echo $this->dashed_name . '-' . $setting; ?>" class="postform" value="<?php echo esc_attr( $value ); ?>" /></div>
+							<?php break;
+							case 'echo': ?>
+								<div><span id="<?php echo $this->dashed_name . '-' . $setting; ?>" class="postform"><?php echo esc_attr( $value ); ?></span></div>
+							<?php break;
+							default: ?>
+								<?php echo $this->settings_texts[$setting]['type']; ?>
+							<?php break;
+						endswitch; ?>
+						<?php if ( !empty( $this->settings_texts[$setting]['desc'] ) ) { echo $this->settings_texts[$setting]['desc']; } ?>
+					</td>
+				</tr>
+				<?php endforeach; ?>
+				<?php if ( 1 == $this->settings['enable'] ): ?>
+					<tr>
+						<td colspan="3">
+							<p>The script has been enabled</p>
+						</td>
+					</tr>
+				<?php endif; ?>
+			</table>
+
+			<p class="submit">
+		<?php
+				if ( function_exists( 'submit_button' ) ) {
+					submit_button( null, 'primary', $this->dashed_name . '-submit', false );
+					echo ' ';
+					submit_button( __( 'Reset to Defaults', $this->plugin_prefix ), '', $this->dashed_name . '-defaults', false );
+				} else {
+					echo '<input type="submit" name="' . $this->dashed_name . '-submit" class="button-primary" value="' . __( 'Save Changes', $this->plugin_prefix ) . '" />' . "\n";
+					echo '<input type="submit" name="' . $this->dashed_name . '-defaults" id="' . $this->dashed_name . '-defaults" class="button-primary" value="' . __( 'Reset to Defaults', $this->plugin_prefix ) . '" />' . "\n";
+				}
+		?>
+			</p>
+
+			</form>
+		</div>
+
+		<?php
+	}
 }
 
-/**
- * Activate the plugin
- */
-function {%= prefix %}_activate() {
-	// First load the init scripts in case any rewrite functionality is being loaded
-	{%= prefix %}_init();
-
-	flush_rewrite_rules();
+// if we loaded wp-config then ABSPATH is defined and we know the script was not called directly to issue a cli call
+if ( defined('ABSPATH') ) {
+	{%= class_name %}::init();
+} else {
+	// otherwise parse the arguments and call the cron.
+	if ( !empty( $argv ) && $argv[0] == basename( __FILE__ ) || $argv[0] == __FILE__ ) {
+		if ( isset( $argv[1] ) ) {
+			echo "You could do something here";
+		} else {
+			echo "Usage: php " . __FILE__ . " <param1>\n";
+			echo "Example: php " . __FILE__ . " superduperparameter\n";
+			exit;
+		}
+	}
 }
-register_activation_hook( __FILE__, '{%= prefix %}_activate' );
 
-/**
- * Deactivate the plugin
- * Uninstall routines should be in uninstall.php
- */
-function {%= prefix %}_deactivate() {
-
-}
-register_deactivation_hook( __FILE__, '{%= prefix %}_deactivate' );
-
-// Wireup actions
-add_action( 'init', '{%= prefix %}_init' );
-
-// Wireup filters
-
-// Wireup shortcodes
